7dc796072074ae66eb1a687a12b8b6a4a49b8c60
surajkadam1308 <159399577+surajkadam1308@users.noreply.github.com>
2024-07-02 10:49:46 +0530

    delet examples file

--- exmaple.js
+++
@@ -1,9 +0,0 @@
-function toggleSettings() {
-    const settingsPanel = document.querySelector('.settings-panel');
-    if (settingsPanel.style.display === 'block') {
-      settingsPanel.style.display = 'none';
-    } else {
-      settingsPanel.style.display = 'block';
-    }
-  }
-  
