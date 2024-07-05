e2069e5b7aa4be200196482a4a16579efc59f95a
surajkadam1308 <159399577+surajkadam1308@users.noreply.github.com>
2024-07-05 22:34:06 +0530

    create a sidebar

---
+++ script.js
@@ -0,0 +1,19 @@
+// function open(){
+//     alert ('hii');
+// }
+// function ok(){
+//    alert('hee');
+// }
+
+function w3_open() {
+    alert();
+    debugger;
+    event.preventDefault()
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("spinner").style.right = "200px";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("spinner").style.right = "200px";
+  }
