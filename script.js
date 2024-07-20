799c44face5092c8ae020ece67e906de1c55bbd7
surajkadam1308 <159399577+surajkadam1308@users.noreply.github.com>
2024-07-20 23:28:02 +0530

    add jqurey functions

--- script.js
+++ script.js
@@ -4,19 +4,35 @@
 // function ok(){
 //    alert('hee');
 // }
+//-------------------------created by s----------------------------
 
-function w3_open() {
-    alert();
-    debugger;
-    event.preventDefault()
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("spinner").style.right = "200px";
-  }
+// function w3_open() {
+
+//    debugger;
+//     event.preventDefault()
+//     document.getElementById("mySidebar").style.display = "block";
+//     document.getElementById("spinner").style.right = "200px";
+//   }
   
-  function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("spinner").style.right = "200px";
-  }
+  //--------------------------------------------
+
+  $(document).ready(function(){
+   
+    $("#spinner").click(function(){
+      $("#mySidebar").toggle("slow")
+      // $("#spinner").css("display","block");
+      $("#spinner").css("right","200px");
+      
+
+    });
+      
+  });
+  
+  //----------------------------------
+
+  
+
+ 
 
 
 
